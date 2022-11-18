bafbed232cb9a9063e7547106493937070ea002e
朽木不可雕也 <aszswaz@163.com>
2022-11-18 22:56:02 +0800

    backup

--- server/bootstrap.go
+++ server/bootstrap.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"giea.aszswaz.cn/aszswaz/shopping/web"
 	"log"
 )
 
 func main() {
-	fmt.Println("Hello World")
 	if err := web.Start(); err != nil {
 		log.Fatalln(err)
 	}
